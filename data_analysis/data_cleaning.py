8c1c53a50a8113180d697d0e22ee3f752100adf2
dasdevidutta3@gmail.com <dasdevidutta3@gmail.com>
2026-06-16 14:45:56 +0530

    Extra Line Delete

--- data_analysis/data_cleaning.py
+++ data_analysis/data_cleaning.py
@@ -1,16 +1,13 @@
 from load_dataset import explore_data_set
 import pandas as pd
 def cleaning_data(matches,deliveries):
-    print("Matches Missing Values \n")
-    print(matches.isnull())
+    
     print("\n" + "==="*50)
     print("The Sum Of Null Values Of Matches \n")
     print("\n" + "==="*50) 
     print(matches.isnull().sum())
 
-    print("\n" + "==="*50) 
-    print("Deliveries Missing Values \n")
-    print(deliveries.isnull())
+
 
     print("\n" + "==="*50)
     print("The Sum Of Null Values Of Deliveries \n")
@@ -18,16 +15,12 @@ def cleaning_data(matches,deliveries):
     print(deliveries.isnull().sum())
 
     print("\n"+ "----"*50)
-    print("\n The Duplicate Values Of Matches \n")
-    print(matches.duplicated())  
-    print("\n" + "==="*50) 
+  
     print("\n The Sum Of The Duplicate Values Of Matches \n") 
     print(matches.duplicated().sum())
     print("\n" + "==="*50)
 
-    print("\n"+ "----"*50)
-    print("\n The Duplicate Values Of Deliveries \n")
-    print(deliveries.duplicated())  
+  
     print("\n" + "==="*50) 
     print("\n The Sum Of The Duplicate Values Of Deliveries \n") 
     print(deliveries.duplicated().sum())
